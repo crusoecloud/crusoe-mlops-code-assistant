18fd94297454c57c69dc1b943afb326a3a981e16
szymon.klimowicz <szymon.klimowicz@deepsense.ai>
2025-05-30 19:58:24 +0200

    feat: update prom-grafana stack, add persistence for grafana

--- kubernetes/prometheus/install-prometheus.sh
+++ kubernetes/prometheus/install-prometheus.sh
@@ -11,15 +11,9 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
 # Install the kube-prometheus-stack v48.2.1 helm chart with `overrides.yaml` file.
 # https://github.com/prometheus-community/helm-charts/tree/kube-prometheus-stack-48.2.1/charts/kube-prometheus-stack
-helm --namespace prometheus-system install prometheus prometheus-community/kube-prometheus-stack --create-namespace --version 48.2.1 -f "${DIR}"/overrides.yaml
-
-# set the place of monitor files
-monitor_dir="${DIR}"/prometheus
-
-# start to install monitor
-pushd "${monitor_dir}"
-for file in *
-do
-kubectl apply -f "${file}"
-done
-popd
+helm \
+upgrade --install \
+--namespace monitoring \
+prometheus prometheus-community/kube-prometheus-stack \
+--version 72.7.0 \
+-f "${DIR}"/values.yaml
